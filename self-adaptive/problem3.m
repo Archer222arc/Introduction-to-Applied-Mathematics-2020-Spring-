8084440d4ad96f7f71a52b39cecc7f1c6c0efc67
Ruicheng Ao <1900012179@pku.edu.cn>
2020-06-25 01:58:05 +0800

    把测试用函数改了

--- self-adaptive/problem3.m
+++ self-adaptive/problem3.m
@@ -13,9 +13,9 @@ end
 grid = grid(m,:);
 k = 0;
 opt.tol = 1e-6;
-opt.theta = 0.8;
+opt.theta = 0.1;
 flag = 0;
-f = @(x,y) power(x^2+y^2,1/3)*mod(atan2(y,x),pi*2)*(1-x^2)*(1-y^2);
+f = @(x,y) power(x^2+y^2,1/3)*sin(2*mod(atan2(y,x),pi*2)/3)*(1-x^2)*(1-y^2);
 while flag == 0
     k = k+1;
     [L,u] = Generate_Dif_adap(grid);
@@ -26,7 +26,11 @@ while flag == 0
         z(i) = f(grid(i,1),grid(i,2));
     end
     err = z-x;
+    e = norm(err);
     output = grid_up(grid,x,opt);    
     flag = output.flag;
     grid = output.grid;
+    if k == 20
+        c = 1;
+    end
  end
