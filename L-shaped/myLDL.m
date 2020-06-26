f5e693248d4b284db32d55ace9139d6b6593b1e5
Ruicheng Ao <1900012179@pku.edu.cn>
2020-06-26 13:12:59 +0800

    Update myLDL.m

--- L-shaped/myLDL.m
+++ L-shaped/myLDL.m
@@ -20,20 +20,22 @@ else
         if i+subsize > n
             [L,D] = myLDL(A(i:n,i:n),opt);
             A(i:n,i:n) = L; 
-            for j = i:n
-                  A(j,j) = D(j);
+            for j = 1:n-i+1
+                  A(j+i-1,j+i-1) = D(j);
             end
         else
             [L,D] = myLDL(A(i:i+subsize-1,i:i+subsize-1));
-            A_res = Tril_eq(L,A(i:i+subsize-1,i+subsize:n));
+            A_res = Tril_eq(L,A(i:i+subsize-1,i+subsize:n))';
             A(i:i+subsize-1,i:i+subsize-1) = L;
+            B = A_res;
             for j = 1:subsize
-                 A_res(j,:) = A_res(j,:)/D(j);
+                 A_res(:,j) = A_res(:,j)/D(j);
                  A(i+j-1,i+j-1) = D(j);
             end
-            A_res = Triu_eq(L',A(i:i+subsize-1,i+subsize:n));
+%             A_res = Triu_eq(L',A(i:i+subsize-1,i+subsize:n))';
+%             A(i+subsize:n,i:i+subsize-1) = A_res*L;
             A(i+subsize:n,i:i+subsize-1) = A_res;
-            A(i+subsize:n,i+subsize:n) = A(i+subsize:n,i+subsize:n) - A_res*A(i:i+subsize-1,i+subsize:n);
+            A(i+subsize:n,i+subsize:n) = A(i+subsize:n,i+subsize:n) - B*A_res';
         end
     end
     L = tril(A,-1)+speye(n); D = diag(A);
