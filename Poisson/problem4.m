5d784f43de5cda0fb953de133bc1b5a4b8050eea
Ruicheng Ao <1900012179@pku.edu.cn>
2020-07-10 01:23:19 +0800

    最终稿

--- Poisson/problem4.m
+++ Poisson/problem4.m
@@ -3,7 +3,7 @@
 %     N = 2^i;
     N = 128;
     opt.h = 1/N;
-    opt.k = 1/512;
+    opt.k = 1/128;
 %     opt.k = 1/(4*N^2);
     opt.time = 1;
 %     n = N-1;
@@ -15,28 +15,23 @@
 %     opt.recursive = 0;
 %     f=@(x,y)exp(-2*pi.^2).*sin(pi*x).*sin(pi*y);
 %     output = Implicit(U_0,opt);    
-    tic;
+      tic;
 %     output = V_cycle(A,U_0,U_0,opt);    
 %     output = Implicit_Chol(U_0,opt);
       output = Implicit_GS(U_0,opt);
-    cost = toc;
+      cost = toc;
 %     output = myGS(A,U_Kron,opt,U_Kron);
-  
-
-    % U = reshape(output.u,[n,n]);
+% U = reshape(output.u,[n,n]);
 %     U1 = zeros(n+2,n+2);
 %     U1(2:n+1,2:n+1) = U;
 %     x = (1:grid_size(2)+2)'*opt.h;
 %     y = (1:grid_size(1)+2)'*opt.h;    
 %     output = Explicit(U_0,opt);
 %     U = output.U;
-%     err(i,1) = relative_err(f,U).e1;
 %     output = Implicit(U_0,opt);
 %     U = output.U;
-%     err(i,2) = relative_err(f,U).e1;
 %     output = myCN(U_0,opt);
 %     U = output.U;
-%     err(i,3) = relative_err(f,U).e1;
 %     surf(x,y,reshape(U1,grid_size+2));
 %     shading interp;
 %     drawnow;
